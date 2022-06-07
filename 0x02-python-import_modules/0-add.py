5abee2aa29559d538ca3f882bdb84dbcfe0b0f14
webwatson92 <webwatson92@gmail.com>
2022-06-07 04:54:53 +0200

    Update 0-add file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # 0_add.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
-
+# <elhadjyoussouf@gmail.com>
+from add_0 import add 
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
+    """Print the sum of 1 and 2."""  
 
     a = 1
     b = 2
