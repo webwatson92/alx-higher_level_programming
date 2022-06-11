ea3a1a266530c47e8152999dfba9d3686efd9649
webwatson92 <webwatson92@gmail.com>
2022-06-11 03:46:00 +0200

    update square file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 0-square_matrix_simple.py
-# Gedeon Obae Gekonge <gideonobae@gmail.com>
+# ouattara el hadj youssouf <elhadjyoussouf@gmail.com>
 
 
 def square_matrix_simple(matrix=[]):
