0efb0361f9fe3009686d532a70fac36f9ab779cc
webwatson92 <webwatson92@gmail.com>
2022-08-11 03:59:44 +0100

    Records are ordered by descending count.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Lists the number of records with the same score in the table second_table.
 -- Records are ordered by descending count.
-SELECT `score`, COUNT(*) AS `name`
+SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
 GROUP BY `score`
-ORDER BY `name` DESC;
+ORDER BY `number` DESC;
