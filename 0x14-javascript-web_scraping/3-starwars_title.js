57e9966bf2913b9ae21beaeb79acf9612eb3b84e
webwatson92 <webwatson92@gmail.com>
2022-10-20 14:31:51 +0000

    fix api endpoint

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
+const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, _, body) {
   console.log(error || JSON.parse(body).title);
 });
